4b53e3ae91c14b8eb6ad06b6221490172f4f7c10
Ze YUAN <zeyuan0611@gmail.com>
2025-10-17 20:43:40 -0400

    Refactor: improve run script for greater flexibility and usability

--- run.sh
+++ run.sh
@@ -5,12 +5,16 @@ source /home/zy3722/miniconda3/etc/profile.d/conda.sh
 conda activate graspgen
 
 # Define variables
-MESH_FILE="${PWD}/models/sample_data/meshes/box.obj"
-MESH_SCALE="1.0"
+OBJECT_NAME="clamp"
+MESH_FILE="${PWD}/models/sample_data/meshes/${OBJECT_NAME}.obj"
+MESH_SCALE="0.001"
 GRIPPER_CONFIG="${PWD}/models/checkpoints/graspgen_robotiq_2f_140.yml"
 SCENE_MESH_FILE="${PWD}/scene_mesh/table.glb"
 GRIPPER_MESH_FILE="robotiq_2f_140"
-OUTPUT_FILE="${PWD}/outputs/output_grasps.yml"
+OUTPUT_FILE="${PWD}/outputs/${OBJECT_NAME}_grasps.yml"
+NUM_SAMPLE_POINTS="2000"
+NUM_GRASPS="500"
+TOPK_NUM_GRASPS="200"
 
 # Run the Python script
 python "${PWD}/scripts/demo_object_mesh.py" \
@@ -20,4 +24,8 @@ python "${PWD}/scripts/demo_object_mesh.py" \
     --scene_mesh_file "$SCENE_MESH_FILE" \
     --gripper_mesh_file "$GRIPPER_MESH_FILE" \
     --filter_collisions \
-    --output_file "$OUTPUT_FILE"
+    --output_file "$OUTPUT_FILE" \
+    --num_sample_points "$NUM_SAMPLE_POINTS" \
+    --num_grasps "$NUM_GRASPS" \
+    --return_topk \
+    --topk_num_grasps "$TOPK_NUM_GRASPS"
